22e54c3dfd98bee13d7d9016e96161ab6f53beed
Sardy222 <renzelflores222@gmail.com>
2025-03-20 11:32:28 +0800

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('aqua-track-cache').then(cache => {
-    return cache.addAll([
-        '/Aqua-Track/',
-        '/Aqua-Track/index.html',
-        '/Aqua-Track/style.css',
-        '/Aqua-Track/script.js',
-        '/Aqua-Track/icon-192.png',
-        '/Aqua-Track/icon-512.png',
-        '/Aqua-Track/service-worker.js'
+            return cache.addAll([
+                '/Aqua-Track/',
+                '/Aqua-Track/index.html',
+                '/Aqua-Track/style.css',
+                '/Aqua-Track/script.js',
+                '/Aqua-Track/icon-192.png',
+                '/Aqua-Track/icon-512.png',
+                '/Aqua-Track/manifest.json'
             ]);
-        });
+        })
     );
 });
 
